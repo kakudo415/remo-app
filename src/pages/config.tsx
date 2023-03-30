cadf511ffe8b3912b2e580591bce3172c35a2fcd
Kakudo Kentaro <31888089+kakudo415@users.noreply.github.com>
2023-03-30 22:47:03 +0900

    feat: Implement Light Signal

---
+++ src/pages/config.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Config() {
+  return (
+    <>
+      <header>
+        <Link href='/'>戻る</Link>
+      </header>
+      <main>
+        <h1>Config</h1>
+      </main>
+    </>
+  )
+}
